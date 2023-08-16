8b66ab50f8c2fbe6d00df78656b54ccbc26c8dd3
Bruno Stangarlin <bruno214214@gmail.com>
2023-08-16 15:16:08 -0300

    Melhorias

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,10 +1,9 @@
 ﻿using BookStore.Data;
 using BookStore.Entities;
+using BookStore.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Distributed;
-using NuGet.Protocol;
-using System.Text.Json;
 
 namespace BookStore.Controllers
 {
@@ -12,87 +11,59 @@ namespace BookStore.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
-        private readonly IDistributedCache _cache;
+        private readonly CacheService _cacheService;
         private readonly DatabaseContext _context;
 
         public BooksController(IDistributedCache cache, DatabaseContext context)
         {
-            _cache = cache;
+            _cacheService = new CacheService(cache);
             _context = context;
         }
 
-        // GET: api/Books
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        public async Task<IActionResult> GetBooks()
         {
-            if (_context.Books == null)
+            var key = "Books:All";
+            var cachedBooks = await _cacheService.GetFromCacheAsync<IEnumerable<Book>>(key);
+
+            if (cachedBooks != null)
             {
-                return NotFound();
+                return Ok(cachedBooks);
             }
 
-            // Redis
-            string key = "Books: All"; // chave para armazenar e recuperar a lista de todos os livros
-            string? booksString = await _cache.GetStringAsync(key);
+            var books = await _context.Books.ToListAsync();
 
-            if (booksString != null)
+            if (books.Any())
             {
-                IEnumerable<Book> _books = JsonSerializer.Deserialize<IEnumerable<Book>>(booksString)!;
-                await Console.Out.WriteLineAsync($"\n[Cache Redis] \nKey = \"{key}\" \nData = {_books.ToJson()}\n");
-
-                return Ok(_books);
+                await _cacheService.SetCacheAsync(key, books);
             }
-
-            //SQL Server
-            IEnumerable<Book>? books = await _context.Books.ToListAsync();
-
-            if (books == null)
-            {
-                return NotFound();
-            }
-
-            booksString = JsonSerializer.Serialize(books);
-            await _cache.SetStringAsync(key, booksString);
 
             return Ok(books);
         }
 
-        // GET: api/Books/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Book>> GetBook(int id)
+        public async Task<IActionResult> GetBook(int id)
         {
-            if (_context.Books == null)
+            var key = $"Book:{id}";
+            var cachedBook = await _cacheService.GetFromCacheAsync<Book>(key);
+
+            if (cachedBook != null)
             {
-                return NotFound();
+                return Ok(cachedBook);
             }
 
-            // Redis
-            string key = $"Book: {id}"; // chave para armazenar e recuperar um livro pelo id
-            string? bookString = await _cache.GetStringAsync(key);
-
-            if (bookString != null)
-            {
-                Book _book = JsonSerializer.Deserialize<Book>(bookString)!;
-                await Console.Out.WriteLineAsync($"\n[Cache Redis] \nKey = \"{key}\" \nData = {_book.ToJson()}\n");
-
-                return Ok(_book);
-            }
-
-            //SQL Server
-            Book? book = await _context.Books.FindAsync(id);
+            var book = await _context.Books.FindAsync(id);
 
             if (book == null)
             {
                 return NotFound();
             }
 
-            bookString = JsonSerializer.Serialize(book);
-            await _cache.SetStringAsync(key, bookString);
+            await _cacheService.SetCacheAsync(key, book);
 
             return Ok(book);
         }
 
-        // PUT: api/Books/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutBook(int id, Book book)
         {
@@ -107,19 +78,15 @@ namespace BookStore.Controllers
             {
                 await _context.SaveChangesAsync();
 
-                // Redis
-                string key = $"Book: {id}"; // chave para encontrar e substituir o livro no cache
-                string bookString = JsonSerializer.Serialize(book);
-                await _cache.SetStringAsync(key, bookString); // atualiza o livro no cache
+                var key = $"Book:{id}";
+                await _cacheService.SetCacheAsync(key, book);
 
-                key = "Books: All"; // chave para invalidar a lista de todos os livros no cache
-                await _cache.RemoveAsync(key); // remove a lista do cache para que a próxima chamada da função GetBooks() busque os dados atualizados do banco de dados
-
-
+                key = "Books:All";
+                await _cacheService.RemoveCacheAsync(key);
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!BookExists(id))
+                if (!await BookExistsAsync(id))
                 {
                     return NotFound();
                 }
@@ -131,37 +98,24 @@ namespace BookStore.Controllers
             return NoContent();
         }
 
-        // POST: api/Books
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Book>> PostBook(Book book)
+        public async Task<IActionResult> PostBook(Book book)
         {
-            if (_context.Books == null)
-            {
-                return Problem("Entity set 'DatabaseContext.Books'  is null.");
-            }
             _context.Books.Add(book);
             await _context.SaveChangesAsync();
 
-            // Redis
-            string key = $"Book: {book.Id}"; // chave para salvar o livro no cache
-            string bookString = JsonSerializer.Serialize(book);
-            await _cache.SetStringAsync(key, bookString); // salva o livro no cache
+            var key = $"Book:{book.Id}";
+            await _cacheService.SetCacheAsync(key, book);
 
-            key = "Books: All"; // chave para invalidar a lista de todos os livros no cache
-            await _cache.RemoveAsync(key); // remove a lista do cache para que a próxima chamada da função GetBooks() busque os dados atualizados do banco de dados
+            key = "Books:All";
+            await _cacheService.RemoveCacheAsync(key);
 
-            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
         }
 
-        // DELETE: api/Books/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteBook(int id)
         {
-            if (_context.Books == null)
-            {
-                return NotFound();
-            }
             var book = await _context.Books.FindAsync(id);
             if (book == null)
             {
@@ -171,20 +125,18 @@ namespace BookStore.Controllers
             _context.Books.Remove(book);
             await _context.SaveChangesAsync();
 
-            // Redis
-            string key = $"Book: {id}"; // chave para encontrar e remover o livro do cache
-            await _cache.RemoveAsync(key); // remove o livro do cache
+            var key = $"Book:{id}";
+            await _cacheService.RemoveCacheAsync(key);
 
-            key = "Books: All"; // chave para invalidar a lista de todos os livros no cache
-            await _cache.RemoveAsync(key); // remove a lista do cache para que a próxima chamada da função GetBooks() busque os dados atualizados do banco de dados
+            key = "Books:All";
+            await _cacheService.RemoveCacheAsync(key);
 
             return NoContent();
         }
 
-        private bool BookExists(int id)
+        private async Task<bool> BookExistsAsync(int id)
         {
-            return (_context.Books?.Any(e => e.Id == id)).GetValueOrDefault();
+            return await _context.Books.AnyAsync(e => e.Id == id);
         }
     }
 }
-
---
+++ Services/CacheService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace BookStore.Services
+{
+    public class CacheService
+    {
+        private readonly IDistributedCache _cache;
+
+        public CacheService(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<T?> GetFromCacheAsync<T>(string key)
+        {
+            string? cachedData = await _cache.GetStringAsync(key);
+            return cachedData != null ? JsonSerializer.Deserialize<T>(cachedData) : default;
+        }
+
+        public async Task SetCacheAsync<T>(string key, T data)
+        {
+            string serializedData = JsonSerializer.Serialize(data);
+            await _cache.SetStringAsync(key, serializedData);
+        }
+
+        public async Task RemoveCacheAsync(string key)
+        {
+            await _cache.RemoveAsync(key);
+        }
+    }
+}
